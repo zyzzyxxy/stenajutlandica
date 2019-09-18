cb2b85116509d041c121e5ebd25ce2d70f520f05
Sajttam <mattias.kallstrom@live.se>
2019-09-18 15:34:01 +0200

    Add files via upload

---
+++ Jutlandica/src/edu/jutlandica/client/StenaFerries.java
@@ -0,0 +1,76 @@
+package edu.jutlandica.client;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class StenaFerries {
+	public final static String JUTLANDICA = "Jutlandica";
+	public final static String DANICA = "Danica";
+	public final static String VINGA = "Vinga";
+	public final static String GERMANICA = "Germanica";
+
+	private static ArrayList<StenaFerries> ferries;
+
+	private String ferry;
+	private String destination;
+
+	private int departureHour;
+	private int departureMinute;
+
+	private int arrivalHour;
+	private int arrivalMinute;
+
+	private StenaFerries(String ferry, String destination, int departureHour, int departureMinute, int arrivalHour,
+			int arrivalMinute) {
+		this.ferry = ferry;
+		this.destination = destination;
+		this.departureHour = departureHour;
+		this.departureMinute = departureMinute;
+		this.arrivalHour = arrivalHour;
+		this.arrivalMinute = arrivalMinute;
+	}
+
+	private static void init() {
+		ferries = new ArrayList<StenaFerries>();
+		ferries.add(new StenaFerries(JUTLANDICA, StenaJutlandica.FREDRIKSHAMN, 0, 30, 4, 10));
+		ferries.add(new StenaFerries(DANICA, StenaJutlandica.FREDRIKSHAMN, 9, 10, 12, 30));
+		ferries.add(new StenaFerries(VINGA, StenaJutlandica.FREDRIKSHAMN, 11, 0, 14, 45));
+		ferries.add(new StenaFerries(JUTLANDICA, StenaJutlandica.FREDRIKSHAMN, 16, 0, 19, 25));
+		ferries.add(new StenaFerries(DANICA, StenaJutlandica.FREDRIKSHAMN, 18, 20, 21, 50));
+		ferries.add(new StenaFerries(VINGA, StenaJutlandica.FREDRIKSHAMN, 22, 10, 1, 30));
+		ferries.add(new StenaFerries(GERMANICA, StenaJutlandica.KIEL, 18, 45, 9, 15));
+	}
+
+	public static StenaFerries getNextDeparture(int hour, int minute, String destination) {
+		if (ferries == null)
+			init();
+
+		StenaFerries ferry = null;
+		int valueHour = Integer.MAX_VALUE;
+		int valueMinute = Integer.MAX_VALUE;
+		for (StenaFerries f : ferries) {
+			int fHour = f.departureHour - hour;
+			int fMinute = f.departureMinute - minute;
+
+			if (f.destination.equals(destination))
+				if (fHour > 0 || (fHour == 0 && fMinute > 0)) {
+					if (fHour < valueHour || (fHour == valueHour && fMinute < valueMinute)) {
+						ferry = f;
+						valueHour = fHour;
+						valueMinute = fMinute;
+					}
+				}
+		}
+
+		return ferry;
+	}
+	
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append(ferry);
+		s.append(", departure: " + departureHour + ":" + departureMinute);
+		s.append(", arrival: " + arrivalHour + ":" + arrivalMinute);
+		s.append(", " + destination);
+		return s.toString();
+	}
+}
--- Jutlandica/src/edu/jutlandica/client/StenaJutlandica.java
+++ Jutlandica/src/edu/jutlandica/client/StenaJutlandica.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package edu.jutlandica.client;
 
 import java.time.LocalTime;
@@ -67,3 +68,87 @@ public class StenaJutlandica implements EntryPoint {
 	}
 
 }
+=======
+package edu.jutlandica.client;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.TimeZone;
+
+import java.util.Date;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class StenaJutlandica implements EntryPoint {
+
+	public static final String USER_FORM = "userForm";
+	
+	public static final String KIEL = "Kiel";
+	public static final String FREDRIKSHAMN = "Fredrikshamn";
+
+	public static final String DEPARTURE_DENMARK = "20:30";
+	public static final String DEPARTURE_GERMANY = "16:25";
+
+	/**
+	 * 
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		final HorizontalPanel formPanel = new HorizontalPanel();
+		final VerticalPanel vPanel = new VerticalPanel();
+		final TextBox from = new TextBox();
+		final Label ferryLabel = new Label("");
+		final Label busLabel = new Label("");
+		
+		// from.setStyleName("textBoxOne");
+
+		// Add a drop box with the list types
+		final ListBox to = new ListBox();
+		to.addItem(KIEL);
+		to.addItem(FREDRIKSHAMN);
+		
+		Button btn = new Button("Submit", new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				BusDepartures bus = null;
+				StenaFerries ferry = null; 
+				
+				Date date = new Date();
+				DateTimeFormat dtf = DateTimeFormat.getFormat("HH:mm");
+				
+				if (to.getSelectedValue().equals(KIEL)) {
+					bus = BusDepartures.getNextDeparture(date.getHours(), date.getMinutes(), BusDepartures.TYSKLAND);
+					ferry = StenaFerries.getNextDeparture(date.getHours(), date.getMinutes(), KIEL);
+					//infoLabel.setText(dtf.format(date, TimeZone.createTimeZone(4)) + " Next departue to Kiel at " + DEPARTURE_GERMANY.toString() + ", Take bus " + buss.toString());
+				}
+				else {
+					bus = BusDepartures.getNextDeparture(date.getHours(), date.getMinutes(), BusDepartures.DANMARK);
+					ferry = StenaFerries.getNextDeparture(date.getHours(), date.getMinutes(), FREDRIKSHAMN);
+					
+					//infoLabel.setText(dtf.format(date, TimeZone.createTimeZone(4)) + " Next departue to Kiel at " + DEPARTURE_DENMARK.toString() + ", Take bus " + buss.toString());
+				}
+				
+				busLabel.setText("BUS: " + bus.toString());
+				ferryLabel.setText("FERRY: " + ferry.toString());
+			}
+		});
+
+		formPanel.add(from);
+		formPanel.add(to);
+		formPanel.add(btn);
+		vPanel.add(formPanel);
+		vPanel.add(busLabel);
+		vPanel.add(ferryLabel);
+		
+		RootPanel.get(USER_FORM).add(vPanel);
+	}
+
+}
+>>>>>>> Stashed changes
