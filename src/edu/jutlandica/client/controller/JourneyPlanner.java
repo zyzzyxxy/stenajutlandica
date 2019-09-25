4673ba4af822135bb7d6b917b24f9276f8d2aa17
zyzzyxxy <joherics@student.chalmers.se>
2019-09-25 13:50:41 +0200

    Backboen fixed wednesday after lunch

---
+++ src/edu/jutlandica/client/controller/JourneyPlanner.java
@@ -0,0 +1,19 @@
+package edu.jutlandica.client.controller;
+
+import java.util.List;
+
+import edu.jutlandica.client.dataclasses.Journey;
+
+public class JourneyPlanner implements InputInterface{
+
+	@Override
+	public List<Journey> getJourneys(String from, String to, String time, String date) {
+		// TODO Auto-generated method stub
+		//Todo get journeys from VT
+		//Todo get journeys from Ferries
+		//Todo Combine journeys
+		return null;
+	}
+	
+
+}
