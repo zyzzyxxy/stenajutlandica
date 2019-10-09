9a55749064b95a6c908d859d0ae49f209f8badaf
Mattias <mattias.kallstrom@live.se>
2019-10-09 09:37:09 +0200

    Deleted everything
    Trying for a new start #ANUSTART

--- src/edu/jutlandica/client/dataclasses/Journey_Interface.java
+++
@@ -1,8 +0,0 @@
-package edu.jutlandica.client.dataclasses;
-
-import java.util.List;
-
-public interface Journey_Interface {
-    public void addTrip(Object o);
-    public List<Trip> getTripList();
-}
